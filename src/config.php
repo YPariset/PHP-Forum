0b480e9f5f23f069239065bc486161d67ebe23e1
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2022-01-09 14:52:06 +0100

    Reset branch

--- src/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    date_default_timezone_set('Europe/Paris');
-
-    const URI_SERVER = 'mongodb+srv://Parizoo:Mareil95850@cluster0.ackwg.mongodb.net/test?authSource=admin&replicaSet=atlas-xfzeox-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
